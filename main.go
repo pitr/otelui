2a0e959958ac8d2d7bbd6207be6c2c2a6a88103a
☃ pitr <pitr.vern@gmail.com>
2026-02-23 21:07:41 +0100

    log only if DEBUG is set

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"log/slog"
 	"os"
@@ -12,14 +13,18 @@ import (
 )
 
 func main() {
-	f, err := os.OpenFile("debug.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o600)
-	if err != nil {
-		fmt.Printf("error opening file for logging: %s", err)
-		return
-	}
-	defer f.Close()
+	logs := io.Discard
 
-	slog.SetDefault(slog.New(slog.NewTextHandler(f, &slog.HandlerOptions{
+	if os.Getenv("DEBUG") != "" {
+		f, err := os.OpenFile("debug.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o600)
+		if err != nil {
+			fmt.Printf("error opening file for logging: %s", err)
+			return
+		}
+		logs = f
+	}
+
+	slog.SetDefault(slog.New(slog.NewTextHandler(logs, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 	})))
 	log.Default()
